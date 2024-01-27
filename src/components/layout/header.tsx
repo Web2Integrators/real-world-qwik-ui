1417ffbc9563eab1a6b46bb4e83a2b78d68caac2
web2integrators <jay@officeintegrator.com>
2024-01-27 04:57:50 -0500

    testing

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 
-import styles from "./header.module.css";
+
 
 import { useAuthSession } from "~/routes/plugin@auth";
 import SignIn from "../auth/sign-in";
@@ -10,13 +10,16 @@ export default component$(() => {
   const session = useAuthSession();
 
   return (
-    <header class={styles.header}>
+    <header class="flex m-8 bg-gray-500">
+      <h1>{JSON.stringify(session.value)}</h1>
       <ul>
         <li>
-          {session.value?.user?.email}
+          
           {session.value?.user?.email ? (
            
+             <div class="flex m-3">
               <SignOut />
+             </div> 
             
           ) : (
             <SignIn />
