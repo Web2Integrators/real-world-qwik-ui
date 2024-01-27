5b2f86958422be280b399a5c8e57a173c8906bf5
web2integrators <jay@officeintegrator.com>
2024-01-27 05:24:12 -0500

    added discord

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,15 +1,24 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
 import GitHub from "@auth/core/providers/github";
+import Discord from "@auth/core/providers/discord";
 import type { Provider } from "@auth/core/providers";
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
   serverAuth$(({ env }) => ({
     secret: env.get("AUTH_SECRET"),
     trustHost: true,
+
+  
+
+
     providers: [
       GitHub({
         clientId: env.get("GITHUB_ID")!,
         clientSecret: env.get("GITHUB_SECRET")!,
+      }),
+      Discord({
+        clientId: env.get("DISCORD_CLIENT_ID")!,
+        clientSecret: env.get("DISCORD_CLIENT_SECRET")!,
       }),
     ] as Provider[],
   }));
